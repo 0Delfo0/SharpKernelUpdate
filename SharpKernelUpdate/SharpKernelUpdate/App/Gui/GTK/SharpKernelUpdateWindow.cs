7da12491c212906fa4d1e9292ef575ce87cba5ef
0Delfo0 <shatetiel@gmail.com>
2017-03-02 23:46:51 +0100

    TEST

---
+++ SharpKernelUpdate/SharpKernelUpdate/App/Gui/GTK/SharpKernelUpdateWindow.cs
@@ -0,0 +1,20 @@
+﻿using Gtk;
+
+namespace SharpKernelUpdate.App.Gui.GTK
+{
+	public partial class SharpKernelUpdateWindow : Gtk.Window
+	{
+		public SharpKernelUpdateWindow() :
+				base(Gtk.WindowType.Toplevel)
+		{
+
+
+			Build();
+		}
+
+		protected void test(object sender, System.EventArgs e)
+		{
+
+		}
+	}
+}
--- SharpKernelUpdate/SharpKernelUpdate/App/Program.cs
+++ SharpKernelUpdate/SharpKernelUpdate/App/Program.cs
@@ -1,42 +1,49 @@
 ﻿using log4net;
 using SharpKernelUpdate.App.Parser;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using Gtk;
+using SharpKernelUpdate.App.Gui.GTK;
 
 namespace SharpKernelUpdate
 {
-    static class Program
-    {
+	static class Program
+	{
 
-        public static readonly ILog LOG = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        /// <summary>
-        /// Punto di ingresso principale dell'applicazione.
-        /// </summary>
-        /// 
-        [STAThread]
-        static void Main()
-        {
+		public static readonly ILog LOG = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		/// <summary>
+		/// Punto di ingresso principale dell'applicazione.
+		/// </summary>
+		/// 
+		[STAThread]
+		static void Main()
+		{
 
-            LOG.Info("START");
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new SharpKernelUpdateForm());
+			LOG.Info("START");
+			//Application.EnableVisualStyles();
+			//Application.SetCompatibleTextRenderingDefault(false);
+			//Application.Run(new SharpKernelUpdateForm());
 
-            //string Pippo = "v3.111.222.333.";
 
-            //            string[] p = Pippo.Split('.');
 
-            //            foreach (string s in p)
-            //{
-            //Console.WriteLine(s);
-            //}
+			Application.Init();
 
-            Parser Parser = new Parser();
-            Parser.getItems();
-            LOG.Info("STOP");
-        }
-    }
+			var sharpKernelUpdateWindow = new SharpKernelUpdateWindow();
+			sharpKernelUpdateWindow.Show();
+
+			Application.Run();
+
+			//string Pippo = "v3.111.222.333.";
+
+			//            string[] p = Pippo.Split('.');
+
+			//            foreach (string s in p)
+			//{
+			//Console.WriteLine(s);
+			//}
+
+			Parser Parser = new Parser();
+			Parser.getItems();
+			LOG.Info("STOP");
+		}
+	}
 }
